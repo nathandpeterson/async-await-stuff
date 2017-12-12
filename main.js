f25547c5510f000797c97ae47c8223b160dd77f5
Nathan Daniel Peterson <natperson@gmail.com>
2017-12-12 15:48:49 -0800

    use async await for axios request

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
+let axios = require('axios')
 
-function writeSomething(){
-  console.log('this is really something')
+
+// class Delay {
+//   constructor(){
+//
+//   }
+//   async handoff = () => {
+//     const message = await this.takesTime('just passing time')
+//     return message
+//   }
+//   takesTime = (message) => {
+//     setTimeout(this.writeSomething, 1000)
+//   }
+//   writeSomething = (message) => {
+//     console.log(message)
+//   }
+// }
+//
+//
+//
+// let myDelay = new Delay()
+// myDelay.handoff() // Throws an error
+
+
+
+
+
+ async function getMusic () {
+  const result = await axios.get(`https://itunes.apple.com/search?term=beatles&entity=musicVideo`)
+
+  console.log(result.data.results)
 }
 
-function takesTime(){
-  setTimeout(writeSomething, 1000)
-}
-
-
-class Nappie {
-  constructor(){
-
-  }
-  async handoff = () => {
-    const message = await takesTime()
-    return message
-  }
-}
-
-
-
-let myNap = new Nappie()
-myNap.handoff()
+getMusic()
